5e6a80f6ca8287efbf18c2452351782b47898ba1
Dillon Mulroy <dillon.mulroy@gmail.com>
2023-05-26 22:36:08 -0400

    Return from get_layout.py

--- get_layout.py
+++ get_layout.py
@@ -3,7 +3,7 @@ def main():
 
 
 def handle_result(args, result, target_window_id, boss):
-    print(boss.active_tab.current_layout.name) 
+    return boss.active_tab.current_layout.name 
 
 
 handle_result.no_ui = True
